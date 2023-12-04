1754ed18571e86821ef8dcf61ff6af4d1b8fa686
Shubh <shubh@uoguelph.ca>
2023-12-04 15:26:23 -0500

    Plant Tracking App - Flutter/Dart

---
+++ plant_tracker/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'pages/My_Plants_Page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        useMaterial3: true,
+      ),
+      home:
+          HomePage(refreshPlantList: () {  },), //i guess we change this function to have different bodies?
+    );
+  }
+}
